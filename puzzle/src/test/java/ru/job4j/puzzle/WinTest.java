273a3c4836abb07ba8acaa33a29e911b37071fd1
m89040885558 <m89040885558@yandex.ru>
2021-08-21 23:37:35 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -55,4 +55,28 @@ public class WinTest {
         };
         assertThat(Win.check(board), is(false));
     }
+
+    @Test
+    public void whenNotWinV() {
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.check(board), is(false));
+    }
+
+    @Test
+    public void whenNotWinL2() {
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0},
+                {1, 1, 1, 1, 0},
+        };
+        assertThat(Win.check(board), is(false));
+    }
 }
